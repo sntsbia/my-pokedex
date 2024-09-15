6ca16407138b958eb7eca43ce3e26a65f055526b
sntsb <sntsb.doc@gmail.com>
2024-09-15 10:57:42 -0300

    Alterando margem, trocando id para string e adicionando um toString()

--- app/src/main/java/com/sntsb/mypokedex/data/model/dto/PokemonDTO.kt
+++ app/src/main/java/com/sntsb/mypokedex/data/model/dto/PokemonDTO.kt
@@ -2,4 +2,8 @@ package com.sntsb.mypokedex.data.model.dto
 
 data class PokemonDTO(
     val id: Int, val nome: String, val imagem: String
-)
+){
+    override fun toString(): String {
+        return "PokemonDTO(id=$id, nome='$nome', imagem='$imagem')"
+    }
+}
